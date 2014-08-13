b52ea99127941c419aea53f7ea5f38c5faa93759
Ian McMahon <ian.mcmahon@turner.com>
2014-08-12 23:56:43 -0400

    created InfoData structure for return type

--- info.go
+++ info.go
@@ -2,6 +2,9 @@ package osincli
 
 import (
 	"fmt"
+	"errors"
+	"reflect"
+	"strconv"
 	"net/url"
 )
 
@@ -9,6 +12,14 @@ type InfoRequest struct {
 	client 				*Client
 	token				string
 	CustomParameters 	map[string]string
+}
+
+type InfoData struct {
+	TokenType    string
+	AccessToken string
+	RefreshToken string
+	Expiration *int32
+	ResponseData ResponseData
 }
 
 // Creates a new info request
@@ -50,22 +61,63 @@ func (c *InfoRequest) GetInfoUrlWithParams(state string) *url.URL {
 }
 
 
-func (c *InfoRequest) GetInfoData() (*ResponseData, error) {
+func (c *InfoRequest) GetInfoData() (*InfoData, error) {
 	iu := c.GetInfoUrl()
 
-	ret := make(ResponseData)
+	ret := &InfoData{
+		ResponseData: make(ResponseData),
+	}
 
 	// download data
 	m := "POST"
 	if c.client.config.UseGetAccessRequest {
 		m = "GET"
 	}
-	err := downloadData(m, iu, nil, c.client.Transport, ret)
+	err := downloadData(m, iu, nil, c.client.Transport, ret.ResponseData)
 	if err != nil {
 		return nil, err
 	}
 
-	fmt.Printf("Received info response: %v\n", ret)
+	// extract and convert received data
+	token_type, ok := ret.ResponseData["token_type"]
+	if !ok {
+		return nil, errors.New("Invalid parameters received")
+	}
+	ret.TokenType = fmt.Sprintf("%v", token_type)
 
-	return &ret, nil
+	access_token, ok := ret.ResponseData["access_token"]
+	if !ok {
+		return nil, errors.New("Invalid parameters received")
+	}
+	ret.AccessToken = fmt.Sprintf("%v", access_token)
+
+	refresh_token, ok := ret.ResponseData["refresh_token"]
+	if !ok {
+		ret.RefreshToken = ""
+	} else {
+		ret.RefreshToken = fmt.Sprintf("%v", refresh_token)
+	}
+
+	expires_in_raw, ok := ret.ResponseData["expires_in"]
+	if ok {
+		rv := reflect.ValueOf(expires_in_raw)
+		switch rv.Kind() {
+		case reflect.Float64:
+			// encoding/json always convert numbers fo float64
+			ret.Expiration = new(int32)
+			*ret.Expiration = int32(rv.Float())
+		case reflect.String:
+			// if string convert to integer
+			ei, err := strconv.ParseInt(rv.String(), 10, 32)
+			if err != nil {
+				return nil, err
+			}
+			ret.Expiration = new(int32)
+			*ret.Expiration = int32(ei)
+		default:
+			return nil, errors.New("Invalid parameter value")
+		}
+	}
+
+	return ret, nil
 }
